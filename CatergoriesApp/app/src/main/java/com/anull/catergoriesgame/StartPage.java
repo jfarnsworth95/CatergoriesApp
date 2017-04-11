ff8467bdd3064b625dc30585ad170b091cce3af3
Jackson F <jafarns95@gmail.com>
2017-04-10 21:30:41 -0500

    added buttons for start page and string values in resources

--- CatergoriesApp/app/src/main/java/com/anull/catergoriesgame/StartPage.java
+++ CatergoriesApp/app/src/main/java/com/anull/catergoriesgame/StartPage.java
@@ -2,6 +2,7 @@ package com.anull.catergoriesgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartPage extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+    }
+
+    public void startGame(View view) {
+        //start game session
+        //Make call to server
+        //Move to host page
+    }
+
+    public void joinGame(View view) {
+        //move to join page
     }
 }
