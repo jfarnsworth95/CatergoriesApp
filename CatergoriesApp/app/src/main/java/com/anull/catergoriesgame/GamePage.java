80525753ad2b12bd0f218837059de220370e31d3
Jackson F <jafarns95@gmail.com>
2017-04-13 22:11:46 -0500

    added game page, considering adding more game pages. ie: votePage, ScorePage.

---
+++ CatergoriesApp/app/src/main/java/com/anull/catergoriesgame/GamePage.java
@@ -0,0 +1,21 @@
+package com.anull.catergoriesgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jackson on 4/13/2017.
+ */
+public class GamePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        //pull catergories from server
+        //pull letter from server
+        //set timer
+        //send data at end of timer
+        //transition to new page?
+    }
+}
